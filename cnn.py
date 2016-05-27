1c1ca3b5bb0b55b7d09747ac7cfb332fddf4211f
sjtu5140809011@gmail.com <sjtu5140809011@gmail.com>
2016-05-27 19:36:18 +0800

    commit before combine 2 and 3

--- cnn.py
+++ cnn.py
@@ -1,39 +1,56 @@
-from keras.layers import Merge,Convolution2D,MaxPooling2D,Dense,Flatten,Activation
-from keras.models import Sequential
+from keras.layers import Merge,Convolution2D,MaxPooling2D,Dense,Flatten,Activation,Input,merge,Dropout
+from keras.layers.core import Activation
+from keras.models import Sequential,Model
+from keras.utils.visualize_util import plot
 import cPickle
 import numpy
 
 if __name__ == '__main__':
-    left_branch=Sequential()
-    left_branch.add(Convolution2D(32,2,99,input_shape=(1,100,100)))
-    left_branch.add(Activation("relu"))
-    left_branch.add(MaxPooling2D(pool_size=(2,2)))
-    left_branch.add(Activation("relu"))
-    left_branch.add(Flatten())
+    left_branch=Input(shape=(1,100,100,),name="arg1_input")
+    left_branch_convo=(Convolution2D(32,4,99,))(left_branch)
+    # left_branch_acti=(Activation("tanh"))(left_branch_convo)
+    left_branch_pool=(MaxPooling2D(pool_size=(2,1)))(left_branch_convo)
+    left_branch_flatten=(Flatten())(left_branch_pool)
 
-    right_branch=Sequential()
-    right_branch.add(Convolution2D(32,2,99,input_shape=(1,100,100,)))
-    left_branch.add(Activation("relu"))
-    right_branch.add(MaxPooling2D(pool_size=(2,2)))
-    left_branch.add(Activation("relu"))
-    right_branch.add(Flatten())
 
-    merged=Merge([left_branch,right_branch],mode="concat")
-    final_model=Sequential()
-    final_model.add(merged)
-    final_model.add(Dense(10))
+    right_branch=Input(shape=(1,100,100,),name="arg2_input")
+    right_branch_convo=(Convolution2D(32,4,99,))(right_branch)
+    # right_branch_acti=(Activation("tanh"))(right_branch_convo)
+    right_branch_pool=(MaxPooling2D(pool_size=(2,1)))(right_branch_convo)
+    right_branch_flatten=(Flatten())(right_branch_pool)
 
-    left_branch.compile(optimizer="rmsprop",loss="sparse_categorical_crossentropy",metrics=["accuracy"])
-    right_branch.compile(optimizer="rmsprop",loss="sparse_categorical_crossentropy",metrics=["accuracy"])
-    final_model.compile(optimizer="rmsprop",loss="sparse_categorical_crossentropy",metrics=["accuracy"])
+    merged=merge([left_branch_flatten,right_branch_flatten],mode="concat")
+    # merged_dropout=(Dropout(0.9))(merged)
+    merged_dense=(Dense(12,name="cate"))(merged) #12 category
+
+    final_model=Model(input=[left_branch,right_branch],output=[merged_dense])
+    plot(final_model,to_file="model.png")
+
+    # jsonfile=final_model.to_json()
+    # with open("modeljson","w") as f :
+    #     f.write(jsonfile)
+    # f.close()
+
     f=open("arg1_image_100","rb")
     arg1=cPickle.load(f)
     f=open("arg2_image_100","rb")
     arg2=cPickle.load(f)
     f=open("label","rb")
     label=cPickle.load(f)
+    f.close()
 
-    left_branch.fit(arg1,label)
-    right_branch.fit(arg2,label)
-    # todo
-    final_model.fit(None,None,validation_split=0.1)
+
+    final_model.compile(optimizer="rmsprop",loss="sparse_categorical_crossentropy",metrics=["accuracy"])
+    final_model.fit({"arg1_input":arg1,"arg2_input":arg2},{"cate":label})
+    final_model.save_weights("my_model_weights.h5")
+
+    f=open("dev_arg1_image_100","rb")
+    arg1=cPickle.load(f)
+    f=open("dev_arg2_image_100","rb")
+    arg2=cPickle.load(f)
+    f=open("dev_label","rb")
+    label=cPickle.load(f)
+    f.close()
+
+    print "="*9
+    print final_model.evaluate({"arg1_input":arg1,"arg2_input":arg2},{"cate":label})
